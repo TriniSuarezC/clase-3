6a93cb372284e59c996e317761ba75070f90307d
TriniSuarezC <trinisuarezcarreras@gmail.com>
2024-08-28 19:09:59 -0300

    Agregando el contador

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { Pressable, Text, View, StyleSheet } from "react-native";
 import Header from "./encabezado";
+import Contador from "./contador";
 
 export default function Index() {
   return (
     <View>
       <Header/>
+      <Contador/>
       </View>
   );
 }
